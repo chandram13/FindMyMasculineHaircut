d96bdbdcd66474d54cc4ff5a8b84aa06b8c305cd
Marvish Subhas Chandra <mschandr@uci.edu>
2022-08-20 14:09:55 -0700

    Add files via upload

--- main.kts
+++ main.kts
@@ -3,7 +3,38 @@ class MaleHaircuts:
 
 fun malehaircutTypes(){
     haircut = ""
-    if haircut == "side part"
-    if haircut == "mohawk"
+    if haircut == "Side part":
+    print("Good for men who want a classic look or might have thinning hair.")
+    if haircut == "Mohawk":
+    print("You're looking for an edgy urban look.")
+    if haircut == "Undercut":
+    print("Have an undercut at the bottom but have lengthy, flowing hair on the top.")
+    if haircut == "Boxcut":
+    print("Have a short haircut but have a trendy box shape that surrounds your forehead and around the head.")
+    if haircut == "Bowl cut":
+    print("If you want a classic, yet simple haircut that is short and equal around the head.")
+    if haircut == "Long hair":
+    print("A simple and stylish look that requires basic maintenance of being straight downward.")
+    if haircut == "Short messy hair":
+    print("Has somewhat long hair on the top and is short on the rest of the head.")
+    if haircut == "Pompadour":
+    print("If you want longer hair on top and shorter sides. Someone who wants thick hair that's clean around the head.")
+    if haircut == "Buzzcut":
+    print("Essentially a very short haircut that is hair in the bare means.")
+    if haircut == "Spiky hair":
+    print("Appealing for a punk fashion, where spikes are either short or long based on your intentions.")
+    if haircut == "Shaved":
+    print("A look for those who don't want to maintain their hair at all unless keeping one's hair shaved completely.")
+    if haircut == "Caesar":
+    print("Basically, a cut that is short and textured around the head but has puffy hair on the top.")
+    if haircut == "Slick back":
+    print("A haircut that has one's head that is clean around the head, but the hair is mopped on the top.")
+    if haircut == "Short afro":
+    print("Easily maintained that has textured curls around the rest of the head.")
+    if haircut == "Afro":
+    print("Big, puffed up hair that appears as a mushroom-like appearance that is cleanly cut around the rest of the head.")
+    if haircut == "Mullet":
+    print("A better trimmed afro-like haircut that is short on the top and sides.")
+    else: print("Ask the user if they want a variance of the given haircuts provided or ask specifically what they want.")
 }
 
